4f02bb7d45125ef11b0758ddf3d1495ba11af86e
Hasika20 <150715123+Hasika20@users.noreply.github.com>
2024-01-10 16:35:33 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,5 +1,5 @@
-'use strict';
-const {Model} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -7,42 +7,31 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) { 
+    static associate(models) {
       // define association here
     }
-    static addTodo({title, dueDate}){
-      return this.create({ title: title, dueDate: dueDate, completed: false})
+
+    static addTodo({ title, dueDate }) {
+      return this.create({ title: title, dueDate: dueDate, completed: false });
     }
+    static getTodos() {
+      return this.findAll();
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
-    } 
-    static deleteTodo(id) {
-      return this.destroy({ where: { id } });
-    }       
+    }
   }
-  Todo.init({
-    title: {
-       type: DataTypes.STRING,
-       allowNull: false,
-       validate: {
-          notEmpty: true,
-       },
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
     },
-    dueDate: {
-       type: DataTypes.DATEONLY,
-       allowNull: false,
-       validate: {
-          isDate: true,
-       },
-    },
-    completed: {
-       type: DataTypes.BOOLEAN,
-       defaultValue: false,
-    },
- }, {
-    sequelize,
-    modelName: 'Todo',
- });
- 
-  return Todo;  
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+  return Todo;
 };
