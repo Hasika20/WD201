d6133a9bad006884be7efe0c2c86449a8c2844e0
Hasika20 <150715123+Hasika20@users.noreply.github.com>
2024-01-13 23:21:11 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,5 +1,7 @@
+/* eslint-disable*/
 "use strict";
 const { Model, Op } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -9,74 +11,100 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Todo.belongsTo(models.User,{
-        foreignKey: 'userId',
-      })
+      Todo.belongsTo(models.User, {
+        foreignKey: "userId",
+      });
     }
 
-    static addTodo({ title, dueDate, userId }) {
-      return this.create({ title: title, dueDate: dueDate, completed: false, userId });
+    static addTodo({ title, dueDateTime, userId }) {
+      return this.create({
+        title: title,
+        dueDateTime: dueDateTime,
+        completed: false,
+        userId,
+      });
+    }
+
+    static getTodos() {
+      return this.findAll();
     }
 
     markAsCompleted() {
       return this.update({ completed: true });
     }
-
-    static async odue(userId){
-      return this.findAll({
-        where:{
-          dueDate:{[Op.lt]:new Date()},
-          userId,
-          completed:false,
-        }
-      });
-    }
-    static async tdue(userId){
-      return this.findAll({
-        where:{
-          dueDate:{[Op.eqt]:new Date()},
-          userId,
-          completed:false,
-        }
-      });    }
-    static async ldue(userId){
-      return this.findAll({
-        where:{
-          dueDate:{[Op.gt]:new Date()},
-          userId,
-          completed:false,
-        }
-      });
-    }
-    static async remove(id){
-      return this.destroy({
-        where:{
-          id,
-          
-        },
-      });
-    }
-
-    
-    
-
-    static async completedItems(userId){
-      return this.findAll({
-        where:{
-          completed:true,
-          userId,
-        },
-      });
-    }
-
     setCompletionStatus(completed) {
-      return this.update({ completed });
+      if (completed === true) {
+        return this.update({ completed: true });
+      } else {
+        return this.update({ completed: false });
+      }
+    }
+    static completedItem(userId) {
+      return this.findAll({
+        where: {
+          userId,
+          completed: true,
+        },
+      });
+    }
+    // I added the below 3 methods, overdue dueToday and dueLater to implement the todo app in the instaractors way
+    static overdue(userId) {
+      return this.findAll({
+        where: {
+          dueDateTime: {
+            [Op.lt]: new Date(),
+          },
+          userId,
+          completed: false,
+        },
+      });
+    }
+    static dueToday(userId) {
+      return this.findAll({
+        where: {
+          dueDateTime: {
+            [Op.eq]: new Date(),
+          },
+          userId,
+          completed: false,
+        },
+      });
+    }
+    static dueLater(userId) {
+      return this.findAll({
+        where: {
+          dueDateTime: {
+            [Op.gt]: new Date(),
+          },
+          userId,
+          completed: false,
+        },
+      });
+    }
+
+    static async remove(id, userId) {
+      return this.destroy({
+        where: {
+          id,
+          userId,
+        },
+      });
     }
   }
   Todo.init(
     {
-      title: DataTypes.STRING,
-      dueDate: DataTypes.DATEONLY,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          len: 5,
+        },
+      },
+      dueDateTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
       completed: DataTypes.BOOLEAN,
     },
     {
