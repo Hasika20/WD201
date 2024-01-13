2abc0b4793053c2d5ef8bd29e9c2ab6cb816116f
Hasika20 <150715123+Hasika20@users.noreply.github.com>
2024-01-14 00:05:14 +0530

    Update 20240112072321-create-user.js

--- todo-app/migrations/20240112072321-create-user.js
+++ todo-app/migrations/20240112072321-create-user.js
@@ -1,6 +1,6 @@
-/* eslint-disable */
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Users", {
