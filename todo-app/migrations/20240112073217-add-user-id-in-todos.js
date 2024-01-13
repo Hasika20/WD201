f78d003895f95ea6f1f9e3957d11ecf31f3109af
Hasika20 <150715123+Hasika20@users.noreply.github.com>
2024-01-13 08:22:55 +0530

    Update 20240112073217-add-user-id-in-todos.js

--- todo-app/migrations/20240112073217-add-user-id-in-todos.js
+++ todo-app/migrations/20240112073217-add-user-id-in-todos.js
@@ -23,7 +23,6 @@ module.exports = {
      */
   },
 
-  // eslint-disable-next-line no-unused-vars
   async down (queryInterface, Sequelize) {
     await queryInterface.removeColumn('Todos','userId')
     /**
