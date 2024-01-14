1b615ba00b6bf3543136af941535579d11912019
Hasika20 <150715123+Hasika20@users.noreply.github.com>
2024-01-14 13:34:55 +0530

    Update 20230927162227-create-todo.js

--- todo-app/migrations/20230927162227-create-todo.js
+++ todo-app/migrations/20230927162227-create-todo.js
@@ -1,3 +1,45 @@
+/*eslint-disable*/
+/*
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+/*
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Todos', {
+      id: {
+        // this comes from the model file
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY
+      },
+      completed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+ 
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Todos')
+  }
+}
+
+*/
+
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -11,11 +53,6 @@ module.exports = {
       },
       title: {
         type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-          notNull: true,
-          len: 5,
-        },
       },
       dueDate: {
         type: Sequelize.DATEONLY,
